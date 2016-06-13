a8797b5407a44ce60df325b9eb41c74b431dfcdf
zekorius <zekorius@gmail.com>
2016-06-13 03:59:21 +0200

    Validation added to add company form

--- log/views.py
+++ log/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from .models import Companies
 from .forms import AddCompany
 from django.shortcuts import redirect
+from django.http import HttpResponse
+
 
 # Create your views here.
 @login_required(login_url='login/')
@@ -19,13 +21,17 @@ def users_page(request):
 
 @login_required(login_url='login/')
 def strona_testowa(request):
+    validError = False
     if request.method == "POST":
         formAdd = AddCompany(request.POST)
-        if formAdd.is_valid():
-            formAdd = formAdd.save(commit=False)
+        if formAdd.is_valid() and len(str(request.POST.get('nip'))) == 9:
             formAdd.save()
             return redirect('strona_testowa')
+        else:
+            companies = Companies.objects.all().order_by('name')
+            validError = True
+            return render(request, 'strona_testowa.html', {'companies': companies, 'formAdd': formAdd, 'validError': validError})
     else:
         formAdd = AddCompany()
         companies = Companies.objects.all().order_by('name')
-    return render(request,'strona_testowa.html', {'companies': companies, 'formAdd': formAdd})
+    return render(request,'strona_testowa.html', {'companies': companies, 'formAdd': formAdd, 'validError': validError})
