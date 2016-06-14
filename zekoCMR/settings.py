39a165563567b8c44b806e2753e2b2d14e76af2b
zekorius <zekorius@gmail.com>
2016-06-14 16:40:23 +0200

    finale version + really fixed rootdir

--- zekoCMR/settings.py
+++ zekoCMR/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 STATIC_URL = '/static/'
 # Add static files
