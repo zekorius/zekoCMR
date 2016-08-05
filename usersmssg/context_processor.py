cad94c8223e6cbf74589364139ebddaeee033cbc
zekorius <zekorius@gmail.com>
2016-08-05 15:01:44 +0200

    messages dropdown looks like it should from begginings

--- usersmssg/context_processor.py
+++ usersmssg/context_processor.py
@@ -2,7 +2,12 @@ from .models import Message
 from django.contrib.auth.models import User
 
 def inbox_processor(request):
-    user = request.user
-    inbox = Message.objects.filter(mssg_to = user).order_by('-delivery_date')[0:5]
-    unread = Message.objects.filter(mssg_to = user).filter(been_read = False).count()
-    return {'inbox_top': inbox, 'unread': unread}
+    try:
+        user = request.user
+        inbox = Message.objects.filter(mssg_to = user).order_by('-delivery_date')[0:5]
+        unread = Message.objects.filter(mssg_to = user).filter(been_read = False).count()
+        if not user:
+            raise InputError()
+        return {'inbox_top': inbox, 'unread': unread}
+    except:
+        return {'inbox_top': False, 'unread':0}
